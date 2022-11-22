c8033ebfe88631ffb34813986a225fe086117893
Maksym Shemet <max.shemet@gmail.com>
2022-11-22 14:11:21 +0200

    add vitest

---
+++ src/components/__tests__/example.spec.js
@@ -0,0 +1,8 @@
+import { expect, test } from 'vitest';
+
+// import { mount } from '@vue/test-utils';
+// import HelloWorld from '../HelloWorld.vue';
+
+test('sanity test', () => {
+  expect(true).toBe(true);
+});
